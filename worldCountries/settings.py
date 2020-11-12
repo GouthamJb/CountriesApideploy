59b64b5aa500585e1bbac4aaf2df67847c7d8aa1
GouthamJb <gouthamj@qburst.com>
2020-11-12 23:19:38 +0530

    initial commit

--- worldCountries/settings.py
+++ worldCountries/settings.py
@@ -79,16 +79,10 @@ WSGI_APPLICATION = 'worldCountries.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME' : 'countriesdb',
-        'USER' : 'postgres',
-        'PASSWORD' : '17091997',
-        'HOST' : 'localhost',
-        'PORT' : '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
